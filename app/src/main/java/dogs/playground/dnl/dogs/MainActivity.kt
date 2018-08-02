c5ee010ed62683fd0d2636864226def51fa04e5a
Danilo <danspaulo@hotmail.com>
2018-08-01 22:15:56 -0300

    Just a little test, loading text with data binding.

--- app/src/main/java/dogs/playground/dnl/dogs/MainActivity.kt
+++ app/src/main/java/dogs/playground/dnl/dogs/MainActivity.kt
@@ -1,12 +1,19 @@
 package dogs.playground.dnl.dogs
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import dogs.playground.dnl.dogs.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.title.text = "Loading text through data binding"
     }
 }
